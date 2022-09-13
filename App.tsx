bed5a6aaed7bb6b220a466cc7a4165f76162267f
apadol-cksource <106534095+apadol-cksource@users.noreply.github.com>
2022-09-13 23:58:39 +0200

    Adding CKEditor

--- App.tsx
+++ App.tsx
@@ -1,11 +1,25 @@
 import * as React from 'react';
+import { CKEditor } from '@ckeditor/ckeditor5-react';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import CKEditorInspector from '@ckeditor/ckeditor5-inspector';
 import './style.css';
 
 export default function App() {
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+    <div className="App">
+        <h2>Using CKEditor 5 in React</h2>
+        <CKEditor
+            editor={ ClassicEditor }
+            data="<p>Hello from CKEditor 5!</p>"
+            onReady={ editor => {
+                console.log( 'CKEditor React Component is ready to use!', editor );
+                CKEditorInspector.attach( editor );
+            } }
+            onChange={ ( event, editor ) => {
+                const data = editor.getData();
+                console.log( { event, editor, data } );
+            } }
+        />
     </div>
   );
 }
